3ac8c102f56a87be64942cdbae9cdab1aaa4111f
ritik ramuka <ritik.ramuka@gmail.com>
2021-10-27 17:17:16 +0530

    air-pen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import Webcam from "react-webcam";
+import * as tf from "@tensorflow/tfjs";
+import * as handpose from "@tensorflow-models/handpose";
+import "./App.css";
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  async function getHandpose() {
+    // load the pre defined handpose model by tensorflow.js
+    const model = await handpose.load();
+    // console.log("model loaded!");
+
+    setInterval(() => {
+      getData(model);
+    }, 1);
+  }
+  getHandpose();
+
+  async function getData(model) {
+    // check for video stream
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      const video = webcamRef.current.video;
+
+      const hand = await model.estimateHands(video);
+      // console.log(hand);
+
+      const context = canvasRef.current.getContext("2d");
+
+      if (hand.length > 0) {
+        // console.log(hand)
+        hand.forEach((prediction) => {
+          const landmarks = prediction.annotations;
+
+          if (
+            landmarks !== "undefined" &&
+            landmarks !== null &&
+            landmarks["indexFinger"] != null
+          ) {
+            const indexFinger = landmarks["indexFinger"];
+            // console.log(indexFinger[0][1]);
+            // context.beginPath();
+            context.arc(
+              indexFinger[3][0],
+              indexFinger[3][1],
+              0,
+              0,
+              3 * Math.PI
+            );
+            context.fillStyle = "black";
+            context.stroke();
+            // context.stroke();
+            // context.closePath();
+          }
+        });
+      }
+    } else {
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Webcam ref={webcamRef} className="window"></Webcam>
+      <canvas ref={canvasRef} className="window"></canvas>
     </div>
   );
 }
